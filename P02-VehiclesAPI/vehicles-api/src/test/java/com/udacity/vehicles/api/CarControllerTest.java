d0622acee7d04d9436b6052a81ea396e36cbdc6c
imanadetunji <theiman.adetunji@gmail.com>
2023-07-17 11:04:49 -0400

    Added setCondition() to save() method in CarService, added update method to CarControllerTest

--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -4,11 +4,8 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -149,6 +146,23 @@ public class CarControllerTest {
     }
 
     /**
+     * Tests the update operation of a car by ID
+     * @return the updated Car object
+     */
+    @Test
+    public void updateCar() throws Exception {
+        Car car = getUpdatedCar();
+        when(carService.save(any(Car.class))).thenReturn(car);
+        mvc.perform(put(new URI("/cars/" + 1L))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.condition", is(Condition.NEW.name())))
+                .andExpect(jsonPath("$.details.body",is("toyota")))
+                .andExpect(jsonPath("$.details.model", is("Test")));
+    }
+
+    /**
      * Creates an example Car object for use in testing.
      * @return an example Car object
      */
@@ -169,6 +183,26 @@ public class CarControllerTest {
         details.setNumberOfDoors(4);
         car.setDetails(details);
         car.setCondition(Condition.USED);
+        return car;
+    }
+
+    private Car getUpdatedCar() {
+        Car car = new Car();
+        car.setLocation(new Location(38.375172, 26.875061));
+        car.setCondition(Condition.NEW);
+        Details details = new Details();
+        Manufacturer manufacturer = new Manufacturer(102, "Toyota");
+        details.setManufacturer(manufacturer);
+        details.setModel("Test");
+        details.setMileage(32280);
+        details.setExternalColor("red");
+        details.setBody("toyota");
+        details.setEngine("3.6L V6");
+        details.setFuelType("Gasoline");
+        details.setModelYear(2021);
+        details.setProductionYear(2018);
+        details.setNumberOfDoors(4);
+        car.setDetails(details);
         return car;
     }
 }
